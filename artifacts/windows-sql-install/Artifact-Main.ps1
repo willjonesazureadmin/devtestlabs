64b9c5a36ac57c4d543306b19cb45092bf141227
Will Jones <82215652+willjonesazureadmin@users.noreply.github.com>
2021-08-13 17:34:11 +0100

    Update Artifact-Main.ps1

--- artifacts/windows-sql-install/Artifact-Main.ps1
+++ artifacts/windows-sql-install/Artifact-Main.ps1
@@ -1,7 +1,1 @@
- $retCode = Start-Process -FilePath "./SQL2019-SSEI-Dev.exe"  -ArgumentList "/Q /IACCEPTSQLSERVERLICENSETERMS /ACTION='install'" -Wait -PassThru
-
-if ($retCode.ExitCode -ne 0)
-{
-
-    throw "Product installation of failed with exit code: $($retCode.ExitCode.ToString())"    
-}
+choco install sql-server-2019 -y
