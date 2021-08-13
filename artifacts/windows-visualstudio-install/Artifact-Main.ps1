8d91b71b538020f0813dcfa54733fd15ce27c40e
Will Jones <82215652+willjonesazureadmin@users.noreply.github.com>
2021-08-13 15:00:26 +0100

    Update Artifact-Main.ps1

--- artifacts/windows-visualstudio-install/Artifact-Main.ps1
+++ artifacts/windows-visualstudio-install/Artifact-Main.ps1
@@ -1,1 +1,1 @@
-./vs_enterprise.exe --installPath C:\minVS --add Microsoft.VisualStudio.Workload.CoreEditor --passive --norestart
+./vs_enterprise__1608404543.1622558692.exe --installPath C:\minVS --add Microsoft.VisualStudio.Workload.CoreEditor --passive --norestart
