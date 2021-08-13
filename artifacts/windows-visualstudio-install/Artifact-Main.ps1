85ffbbaa206f13f302249154ef8bc4bb4f83eb80
Will Jones <82215652+willjonesazureadmin@users.noreply.github.com>
2021-08-13 17:21:11 +0100

    Update Artifact-Main.ps1

--- artifacts/windows-visualstudio-install/Artifact-Main.ps1
+++ artifacts/windows-visualstudio-install/Artifact-Main.ps1
@@ -1,1 +1,7 @@
-./vs_enterprise__1608404543.1622558692.exe --installPath C:\minVS --add Microsoft.VisualStudio.Workload.CoreEditor --passive --norestart
+$retCode = Start-Process -FilePath "vs_enterprise__1608404543.1622558692.exe"  -ArgumentList "--installPath C:\minVS --add Microsoft.VisualStudio.Workload.CoreEditor --passive --norestart" -Wait -PassThru
+
+if ($retCode.ExitCode -ne 0)
+{
+
+    throw "Product installation of failed with exit code: $($retCode.ExitCode.ToString())"    
+}
