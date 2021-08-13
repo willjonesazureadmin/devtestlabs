f1ebceb2f5977a20385d365b0386de4ad8cdfe26
Will Jones <82215652+willjonesazureadmin@users.noreply.github.com>
2021-08-13 17:20:15 +0100

    Update Artifact-Main.ps1

--- artifacts/windows-sql-install/Artifact-Main.ps1
+++ artifacts/windows-sql-install/Artifact-Main.ps1
@@ -1,1 +1,7 @@
-./SQL2019-SSEI-Dev.exe /Q /IACCEPTSQLSERVERLICENSETERMS /ACTION='install'
+ $retCode = Start-Process -FilePath "./SQL2019-SSEI-Dev.exe"  -ArgumentList "/Q /IACCEPTSQLSERVERLICENSETERMS /ACTION='install'" -Wait -PassThru
+
+if ($retCode.ExitCode -ne 0)
+{
+
+    throw "Product installation of failed with exit code: $($retCode.ExitCode.ToString())"    
+}
