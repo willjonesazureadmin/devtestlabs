3cbc804c9750d425ef3db9e6b927ab73cc8ac5c4
Will Jones <82215652+willjonesazureadmin@users.noreply.github.com>
2021-08-14 08:28:29 +0100

    Update Artifact-Main.ps1

--- artifacts/windows-aad-login/Artifact-Main.ps1
+++ artifacts/windows-aad-login/Artifact-Main.ps1
@@ -1,3 +1,4 @@
 $VM = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET  -Uri "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+Install-Module Az -Force
 Login-AzAccount -Identity
 az vm extension set --publisher Microsoft.Azure.ActiveDirectory --name AADLoginForWindows --resource-group $vm.compute.resourceGroupName --vm-name $vm.compute.name
