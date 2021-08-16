67c0cf24263c8f5c665a735a64760d6bd57a14c3
Will Jones <82215652+willjonesazureadmin@users.noreply.github.com>
2021-08-16 16:10:46 +0100

    Update Artifact-Main.ps1

--- artifacts/windows-azurecli-install/Artifact-Main.ps1
+++ artifacts/windows-azurecli-install/Artifact-Main.ps1
@@ -10,7 +10,7 @@ param(
     [string] $PsModules = $null,
 
     # comma- separated list of powershell modules.
-    [string] $Packages,
+    [string] $Packages "azure-cli",
 
     # Boolean indicating if we should allow empty checksums. Default to true to match previous artifact functionality despite security
     [bool] $AllowEmptyChecksums = $true,
