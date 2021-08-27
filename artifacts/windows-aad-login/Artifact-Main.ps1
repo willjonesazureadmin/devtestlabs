66bca54935938c4b7bd5d0fab4b054a5b976ac31
Will Jones <82215652+willjonesazureadmin@users.noreply.github.com>
2021-08-27 12:59:52 +0100

    Update Artifact-Main.ps1

--- artifacts/windows-aad-login/Artifact-Main.ps1
+++ artifacts/windows-aad-login/Artifact-Main.ps1
@@ -9,7 +9,7 @@ param(
     [string] $PsModules = "Az",
 
     # comma- separated list of powershell modules.
-    [string] $Packages = "azure-cli",
+    [string] $Packages = "azure-cli;",
 
     # Boolean indicating if we should allow empty checksums. Default to true to match previous artifact functionality despite security
     [bool] $AllowEmptyChecksums = $true,
